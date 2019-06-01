a1cb6abe3a0315b03291a579266ed801b480e8d8
Rafael Ritter <rafaritter44@hotmail.com>
2019-05-31 22:31:01 -0300

    Create basic endpoints

---
+++ src/main/java/br/pucrs/ages/mase/disaster_service/dto/RiskAreaDto.java
@@ -0,0 +1,26 @@
+package br.pucrs.ages.mase.disaster_service.dto;
+
+import java.math.BigDecimal;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+public class RiskAreaDto {
+	
+	private @NotNull CenterDto center;
+	private @NotNull BigDecimal radius;
+	private @NotEmpty String disasterType;
+	
+	public CenterDto getCenter() {
+		return center;
+	}
+	
+	public BigDecimal getRadius() {
+		return radius;
+	}
+	
+	public String getDisasterType() {
+		return disasterType;
+	}
+	
+}
